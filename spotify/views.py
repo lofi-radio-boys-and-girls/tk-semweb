035e4ac281f2653fd923f5ae4d2277a695627dfd
Sabyna Maharani <sabyna.maharani@ui.ac.id>
2022-12-22 05:00:48 +0700

    Implement frontend for search page

--- spotify/views.py
+++ spotify/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from spotify.queries.query import get_songs_and_artists
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def search(request):
+    context = {
+        'keyword': request.GET['keyword'],
+    }
+    return render(request, 'search.html', context)
+
+
+def detail(request, song_by_artis):
+    context = {
+        'keyword': song_by_artis,
+    }
+    return render(request, 'detail.html', context)
